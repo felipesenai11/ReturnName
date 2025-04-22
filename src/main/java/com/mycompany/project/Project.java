0821e45fa3f3938c0a7873dc7697460e4602a578
gabri <gabri@Alef>
2025-04-21 23:09:03 -0300

    Name return Project

---
+++ src/main/java/com/mycompany/project/Project.java
@@ -0,0 +1,12 @@
+package com.mycompany.project;
+import java.util.Scanner; 
+public class Project {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+        
+        System.out.println("O seu nome é: ");
+    }
+}
